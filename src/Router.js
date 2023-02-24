3faed0c381f3b886e40583fbffd76417a1895e3a
JamesZhang <tokipona2956@gmail.com>
2023-02-24 11:23:12 +0800

    Update Router.js
    remove event listener

--- src/Router.js
+++ src/Router.js
@@ -32,7 +32,7 @@ class App extends React.Component {
 
   componentWillUnmount() {
     BackHandler.removeEventListener('hardwareBackPress', this.props.backAndroidHandler || this.onBackPress);
-    Linking.removeEventListener('url', this.handleDeepURL);
+    // Linking.removeEventListener('url', this.handleDeepURL);
   }
 
   onBackPress = () => this.props.navigationStore.pop();
